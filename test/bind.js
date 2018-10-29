40ab110cc5dccd756d532b122e0b709617af4fb9
MouseZero <rm@mousezero.com>
2018-10-28 20:55:56 -0700

    More fun with this

---
+++ test/bind.js
@@ -0,0 +1,38 @@
+const {expect} = require('chai')
+
+describe('bind', () => {
+  it('can not bind on top of original bind', () => {
+    const a = {foo: 'bar'}
+    const b = {foo: 'qux'}
+
+    function func () {
+      return this.foo
+    }
+
+    const newFunc = func.bind(a)
+    const newNewFunc = newFunc.bind(b)
+
+    expect(newFunc.apply(b)).to.equal('bar')
+    expect(func.apply(b)).to.equal('qux')
+    expect(newNewFunc()).to.equal('bar')
+  })
+
+  it('this inside function not effected by outside enviornment',
+  function () {
+    const a = {foo: 'bar'}
+
+    global.foo = 'qux'
+
+    function func () {
+      return this.foo
+    }
+
+    ;(function () {
+      expect(this.foo).to.equal('bar')
+      expect(func()).to.equal('qux')
+      expect(func.apply(a)).to.equal('bar')
+    }).apply(a)
+
+
+  })
+})
